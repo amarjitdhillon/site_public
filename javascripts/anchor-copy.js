c8f9d754e2b7521792aa22fb29a47edf93eac40b
Amarjit Singh Dhillon <amar@amarjits-mbp.home>
2023-12-31 17:21:23 -0500

    empty commit

--- javascripts/anchor-copy.js
+++
@@ -1,6 +0,0 @@
-document$.subscribe(() => {
-  const links = Array.from(document.querySelectorAll(".headerlink"))
-  for (const link of links) {
-    link.setAttribute("data-clipboard-text", link.href)
-  }
-})
